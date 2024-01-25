c33aabc71a24dd656ff954869040a2ded8a3910a
mahendra761 <mahendragawande90@gmail.com>
2024-01-25 11:58:14 +0530

    add nav menu

--- App.js/swiper.js
+++ App.js/swiper.js
@@ -35,10 +35,37 @@ MenuItem.style.maxHeight= "0px";
 function menutoggle(){
     if(MenuItem.style.maxHeight == "0px")
     {
-        MenuItem.style.maxHeight = "200px";
+        MenuItem.style.maxHeight = "300px";
     }
     else
     {
         MenuItem.style.maxHeight = "0px";
     }
 }
+
+// form submit
+
+function submitForm(event) {
+  event.preventDefault();
+
+  fetch("https://getform.io/f/f465d4da-98d8-4702-b3a3-0f25b6cb52d6", {
+      method: "POST",
+      body: new FormData(event.target),
+  })
+  .then(response => {
+      if (response.ok) {
+          // Reset the form fields
+          document.getElementById("name").value = "";
+          document.getElementById("email").value = "";
+          document.getElementById("number").value = "";
+          document.getElementById("Message").value = "";
+
+          
+          alert("Form submitted successfully!");
+      }
+  })
+  .catch(error => {
+      console.error("Error submitting form:", error);
+  });
+}
+
